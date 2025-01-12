55882b90853bc1edf4f7be388d002703eeac31da
Soham Kolhatkar <sohamk130405@gmail.com>
2025-01-12 15:34:03 +0530

    increased payload size

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,9 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(cookieParser());
 app.use(
   cors({
@@ -20,6 +22,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
